7f56095b60f5c09c29e0dcd05c9a00d8891344e8
Janeibeh <janewhite.ji@gmail.com>
2024-04-09 10:40:50 +0100

    PROJECT SET UP

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './Components/Home'
+
+const Homepage = () => {
+  return (
+    <div className=''>
+          <Home/>
+    </div>
+
+  )
+}
+
+export default Homepage
